109377316380c13ff6edc5687c5dc8ae458bba17
PJ82 <project82@mail.ru>
2023-10-24 14:30:10 +0500

    Fix

--- src/can_frame.cpp
+++ src/can_frame.cpp
@@ -2,84 +2,84 @@
 #include "tools.h"
 #include "esp32-hal-log.h"
 
-using namespace hardware;
-
-CanFrame::CanFrame() {
-    log_d("Frame created");
-    clear();
-}
-
-void CanFrame::clear() {
-    id = 0x00;
-    length = 0;
-    extended = 0;
-    rtr = 0;
-    self = 0;
-    memset(&data, 0, sizeof(data));
-    log_d("Frame cleared");
-}
-
-int CanFrame::set(twai_message_t message) {
-    log_d("Set message: id: %04x, bytes: %d, data: %s", message.identifier, message.data_length_code, message.data);
-
-    id = message.identifier;
-    length = message.data_length_code;
-    rtr = message.rtr;
-    for (int i = 0; i < length; i++) {
-        data.bytes[i] = message.data[i];
+namespace hardware {
+    CanFrame::CanFrame() {
+        log_d("Frame created");
+        clear();
     }
-    return length;
-}
 
-twai_message_t CanFrame::get() {
-    twai_message_t message;
-    memset(&message, 0, sizeof(message));
-    message.identifier = id;
-    message.data_length_code = length;
-    for (int i = 0; i < length; i++) {
-        message.data[i] = data.bytes[i];
+    void CanFrame::clear() {
+        id = 0x00;
+        length = 0;
+        extended = 0;
+        rtr = 0;
+        self = 0;
+        memset(&data, 0, sizeof(data));
+        log_d("Frame cleared");
     }
-    message.self = self;
-    message.extd = extended;
 
-    log_d("Set message: id: %04x, bytes: %d, data: %s", message.identifier, message.data_length_code, message.data);
-    return message;
-}
+    int CanFrame::set(twai_message_t message) {
+        log_d("Set message: id: %04x, bytes: %d, data: %s", message.identifier, message.data_length_code, message.data);
 
-bool CanFrame::is() const {
-    return id > 0x00 && length > 0 && rtr == 0;
-}
+        id = message.identifier;
+        length = message.data_length_code;
+        rtr = message.rtr;
+        for (int i = 0; i < length; i++) {
+            data.bytes[i] = message.data[i];
+        }
+        return length;
+    }
 
-uint16_t CanFrame::get_word(int index) {
-    if (index >= 0 && index + 1 < length) {
-        uint16_t result = word(data.bytes[index], data.bytes[index + 1]);
-        log_d("Get word: %d", result);
+    twai_message_t CanFrame::get() {
+        twai_message_t message;
+        memset(&message, 0, sizeof(message));
+        message.identifier = id;
+        message.data_length_code = length;
+        for (int i = 0; i < length; i++) {
+            message.data[i] = data.bytes[i];
+        }
+        message.self = self;
+        message.extd = extended;
+
+        log_d("Set message: id: %04x, bytes: %d, data: %s", message.identifier, message.data_length_code, message.data);
+        return message;
+    }
+
+    bool CanFrame::is() const {
+        return id > 0x00 && length > 0 && rtr == 0;
+    }
+
+    uint16_t CanFrame::get_word(int index) {
+        if (index >= 0 && index + 1 < length) {
+            uint16_t result = word(data.bytes[index], data.bytes[index + 1]);
+            log_d("Get word: %d", result);
+            return result;
+        }
+        log_w("Get word: index is outside");
+        return 0;
+    }
+
+    bool CanFrame::compare(CanFrame &frame) {
+        bool result = frame.id == id && frame.length == length;
+        if (result) {
+            for (int i = 0; i < length; i++) {
+                result = frame.data.bytes[i] == data.bytes[i];
+                if (!result) break;
+            }
+        }
+        log_d("Compare: %s", result ? "true" : "false");
         return result;
     }
-    log_w("Get word: index is outside");
-    return 0;
-}
 
-bool CanFrame::compare(CanFrame &frame) {
-    bool result = frame.id == id && frame.length == length;
-    if (result) {
-        for (int i = 0; i < length; i++) {
-            result = frame.data.bytes[i] == data.bytes[i];
-            if (!result) break;
+    bytes_t CanFrame::get_bytes(const int index[], size_t size) {
+        bytes_t result;
+        for (uint8_t &i: result.bytes) i = 0;
+        int idx;
+        for (int i = 0; i < size; i++) {
+            idx = index[i];
+            result.bit[i] = idx >= 0 && idx < 64 && data.bit[idx];
         }
+        log_d("Get bytes: %s", result.bytes);
+        return result;
     }
-    log_d("Compare: %s", result ? "true" : "false");
-    return result;
-}
-
-bytes_t CanFrame::get_bytes(const int index[], size_t size) {
-    bytes_t result;
-    for (uint8_t &i: result.bytes) i = 0;
-    int idx;
-    for (int i = 0; i < size; i++) {
-        idx = index[i];
-        result.bit[i] = idx >= 0 && idx < 64 && data.bit[idx];
-    }
-    log_d("Get bytes: %s", result.bytes);
-    return result;
 }
